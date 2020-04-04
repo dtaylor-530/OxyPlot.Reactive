df74db280554b8cda6c75747513a6be1efde66d7
dtaylor-530 <d.taylor530@gmail.com>
2020-04-04 18:21:59 +0100

    Addsd support for getting the selected item in a plotmodel & added support for viewing a a subset of data in plot in MultiLineModel

--- OxyPlot.Reactive.DemoApp/Views/MultiLineView.xaml.cs
+++ OxyPlot.Reactive.DemoApp/Views/MultiLineView.xaml.cs
@@ -2,6 +2,7 @@
 using OxyPlotEx.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
 using System.Windows;
@@ -19,17 +20,39 @@ namespace OxyPlotEx.DemoAppCore.Pages
     /// <summary>
     /// Interaction logic for LineView.xaml
     /// </summary>
-    public partial class LineView : UserControl
+    public partial class MultiLineView : UserControl
     {
-        public LineView()
+        public MultiLineView()
         {
             InitializeComponent();
 
             plotView.Model = new OxyPlot.PlotModel();
             var model1 = new MultiLineModel<string>(new DispatcherX(this.Dispatcher), plotView.Model) { };
-            //var model2 = new LineModel<string>(new DispatcherX(this.Dispatcher), plotView.Model) { };
             ProduceData().Subscribe(model1);
 
+            plotView2.Model = new OxyPlot.PlotModel();
+            var model2 = new MultiLineModel<string>(new DispatcherX(this.Dispatcher), plotView2.Model) { };
+
+            var array = new DataFactory().GetLine().Skip(100).Take(20).Select((o, i) =>
+             {
+                 return new KeyValuePair<string, (DateTime, double)>(o.Key, (DateTime.UnixEpoch.AddYears(i), o.Value));
+             }).ToArray();
+            array.ToObservable().Subscribe(model2);
+
+            model2.Subscribe(p =>
+            {
+                var n =  array.Select((a,i)=>(a.Value.Item1,i)).Single(a => a.Item1 == p.DateTime).i;
+                DataGrid1.SelectedIndex = n;
+                DataGrid1.ScrollIntoView(DataGrid1.Items[n]);
+            });
+
+            DataGrid1.ItemsSource = array;
+
+
+            plotView3.Model = new OxyPlot.PlotModel();
+            var model3 = new MultiLineModel<string>(new DispatcherX(this.Dispatcher), plotView3.Model) { };
+            Observable.Return(10).Subscribe(model3);
+            ProduceData().Subscribe(model3);
         }
 
         private static IObservable<KeyValuePair<string, (DateTime, double)>> ProduceData()
@@ -50,9 +73,6 @@ namespace OxyPlotEx.DemoAppCore.Pages
             return obs1;
         }
 
-        private void Multi_Line_Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+      
     }
 }
--- OxyPlot.Reactive/MultiLineModel.cs
+++ OxyPlot.Reactive/MultiLineModel.cs
@@ -1,27 +1,30 @@
-﻿
+﻿#nullable enable
+
+using OxyPlot;
+using OxyPlot.Axes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Reactive.Subjects;
+using OxyPlot.Series;
+using MoreLinq;
+
 namespace OxyPlotEx.ViewModel
 {
-    using MoreLinq;
-    using OxyPlot;
-    using OxyPlot.Axes;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reactive;
-    using System.Reactive.Linq;
-    using System.Reactive.Subjects;
-    using System.Reactive.Threading.Tasks;
-    using System.Threading.Tasks;
-    using e = System.Linq.Enumerable;
-
-    public class MultiLineModel<T> : MultiPlotModel<T, DateTime>
+    public class MultiLineModel<T> : MultiPlotModel<T, DateTime>, IObservable<DateTimePoint>, IObserver<int>
     {
+        readonly List<IDisposable> disposables = new List<IDisposable>();
+        readonly Subject<DateTimePoint> subject = new Subject<DateTimePoint>();
+        private int? count;
 
         public MultiLineModel(IDispatcher dispatcher, PlotModel plotModel) : base(dispatcher, plotModel)
         {
         }
 
-        public MultiLineModel(IDispatcher dispatcher, PlotModel model, IEqualityComparer<T> comparer) : base(dispatcher, model,comparer)
+        public MultiLineModel(IDispatcher dispatcher, PlotModel model, IEqualityComparer<T> comparer) : base(dispatcher, model, comparer)
         {
         }
 
@@ -34,51 +37,90 @@ namespace OxyPlotEx.ViewModel
         {
             this.dispatcher.BeginInvoke(async () =>
             {
-                plotModel.Series.Clear();
                 foreach (var keyValue in DataPoints.ToArray())
                 {
-                    await Task.Run(() =>
-                    {
-                        lock (lck)
-                        {
-
-                            var points = keyValue.Value
-                                            .OrderBy(c => c.X)
-                                            .Scan((xy0, xy) => new DataPoint<DateTime>(xy.X, Combine(xy0.Y, xy.Y))).ToArray();
-                            return points;
-                        }
-
-                    }).ContinueWith(async points =>
-                    {
-                        AddToSeries(await points, keyValue.Key.ToString());
-                    }, TaskScheduler.FromCurrentSynchronizationContext());
+                    _ = await Task.Run(() =>
+                      {
+                          lock (lck)
+                          {
+                              return count.HasValue ?
+                               Enumerable.TakeLast(ToDataPoints(keyValue.Value), count.Value).ToArray() :
+                               ToDataPoints(keyValue.Value).ToArray();
+                          }
+                      }).ContinueWith(async points =>
+                      {
+                          AddToSeries(await points, keyValue.Key.ToString());
+                      }, TaskScheduler.FromCurrentSynchronizationContext());
                 }
 
                 if (ShowAll)
                 {
-                    await Task.Run(() =>
-                    {
-                        lock (lck)
-                        {
-                            var allPoints = DataPoints
-                            .SelectMany(a => a.Value)
-                            .OrderBy(c => c.X)
-                            .Scan((xy0, xy) => new DataPoint<DateTime>(xy.X, Combine(xy0.Y, xy.Y)));
-                            return allPoints.ToArray();
-                        }
+                    _ = await Task.Run(() =>
+                     {
+                         lock (lck)
+                             return count.HasValue ?
+                             Enumerable.TakeLast(ToDataPoints(DataPoints.SelectMany(a => a.Value)), count.Value).ToArray() :
+                             ToDataPoints(DataPoints.SelectMany(a => a.Value)).ToArray();
 
-                    }).ContinueWith(async points =>
-                    {
-                        AddToSeries(await points, "All");
-                    }, TaskScheduler.FromCurrentSynchronizationContext());
+                     }).ContinueWith(async points =>
+                     {
+                         AddToSeries((await points), "All");
+                     }, TaskScheduler.FromCurrentSynchronizationContext());
                 }
                 plotModel.InvalidatePlot(true);
             });
+
+            IEnumerable<DateTimePoint> ToDataPoints(IEnumerable<(DateTime X, double Y)> collection)
+                => collection
+                        .OrderBy(c => c.X)
+                        .Scan(new DateTimePoint(), (xy0, xy) => new DateTimePoint(xy.X, Combine(xy0.Value, xy.Y)))
+                        .Skip(1);
         }
 
-        protected virtual void AddToSeries(DataPoint<DateTime>[] points, string title)
+
+        protected virtual void AddToSeries(DateTimePoint[] items, string title)
         {
-            plotModel.Series.Add(OxyFactory.Build(points.Select(a=>new DateTimePoint(a.X,a.Y)), title));
+            if (!(plotModel.Series.SingleOrDefault(a => a.Title == title) is ItemsSeries series))
+            {
+                series = (OxyFactory.Build(items, title));
+
+                //var observable = series.ToMouseDownEvents().Select(e =>
+                //{
+                //    var index = e.HitTestResult.Index;
+                //    // Index of nearest point in LineSeries
+                //    var tt = (int)Math.Round(e.HitTestResult.Index);
+                //    var point = items[tt];
+                //    return point;
+                //});
+                //_ = observable.Subscribe(subject);
+
+                series.MouseDown += (s, e) =>
+                {
+                    var index = e.HitTestResult.Index;
+                    // Index of nearest point in LineSeries
+                    var tt = (int)Math.Round(e.HitTestResult.Index);
+                    var point = items[tt];
+
+                    subject.OnNext(point);
+                };
+
+                plotModel.Series.Add(series);
+
+                //return;
+            }
+
+            series.ItemsSource = items;
+        }
+
+        public IDisposable Subscribe(IObserver<DateTimePoint> observer)
+        {
+            return subject.Subscribe(observer);
+        }
+
+        public void OnNext(int count)
+        {
+            this.count = count;
+            refreshSubject.OnNext(Unit.Default);
         }
     }
 }
