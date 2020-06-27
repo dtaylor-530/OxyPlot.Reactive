ae7f2351c560dabdcbae0c5ff71003ab06ecf224
dtaylor-530 <d.taylor530@gmail.com>
2020-06-27 19:06:10 +0100

    Removes using statements.

--- OxyPlot.Reactive.DemoApp/Views/HeatMapView.xaml.cs
+++ OxyPlot.Reactive.DemoApp/Views/HeatMapView.xaml.cs
@@ -5,16 +5,8 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reactive.Linq;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace OxyPlotEx.DemoAppCore.Pages
 {
