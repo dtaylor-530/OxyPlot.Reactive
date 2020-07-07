87b79366aef481e8e67b548f07b7f2b651b42892
dtaylor-530 <d.taylor530@gmail.com>
2020-07-08 00:07:28 +0100

    Adds MultiDateTimePlotViewModel for collections of plotmodels

---
+++ OxyPlot.Reactive.DemoApp/Views/MultiPlotView.xaml.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using DynamicData;
+using MoreLinq;
+using OxyPlot.Reactive.DemoApp.Common;
+using OxyPlot.Reactive.DemoApp.Factory;
+using ReactiveUI;
+
+namespace OxyPlot.Reactive.DemoApp.Views
+{
+    /// <summary>
+    /// Interaction logic for MultiPlotView.xaml
+    /// </summary>
+    public partial class MultiPlotView : UserControl
+    {
+        private readonly ReadOnlyObservableCollection<PlotModel> plots;
+
+        public MultiPlotView()
+        {
+            InitializeComponent();
+
+            var abc = new string[] { "a", "b", "c" }.Repeat().GetEnumerator();
+
+            var pacedObs = DataSource.Observe1000().Pace(TimeSpan.FromSeconds(0.6)).Select(a =>
+            {
+                abc.MoveNext();
+                return KeyValuePair.Create(abc.Current, a);
+            });
+
+            var mplots = new MultiDateTimePlotModel<string, string>(scheduler: RxApp.MainThreadScheduler);
+            pacedObs.Subscribe(mplots);
+
+            mplots.ToObservableChangeSet()
+                .ObserveOnDispatcher()
+                .SubscribeOnDispatcher()
+                .Bind(out plots)
+                .Subscribe();
+
+            ItemsControl1.ItemsSource = plots;
+
+        }
+    }
+}
---
+++ OxyPlot.Reactive/MultiDateTimePlotModel.cs
@@ -0,0 +1,75 @@
+﻿#nullable enable
+
+using NestedDictionaryLib;
+using OxyPlot.Reactive.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Subjects;
+using System.Threading;
+
+namespace OxyPlot.Reactive
+{
+    public class MultiDateTimePlotModel<TGroupKey, TKey> : IObserver<KeyValuePair<TGroupKey, KeyValuePair<TKey, KeyValuePair<DateTime, double>>>>, IObservable<PlotModel>, IMixedScheduler
+    {
+        protected readonly ISubject<Unit> refreshSubject = new Subject<Unit>();
+        //private readonly IEqualityComparer<TKey>? comparer;
+        //private readonly IScheduler? scheduler;
+        protected Dictionary<TGroupKey, MultiDateTimeModel<TKey>> Models = new Dictionary<TGroupKey, MultiDateTimeModel<TKey>>();
+        protected Subject<PlotModel> PlotModelChanges = new Subject<PlotModel>();
+        private IEqualityComparer<TKey>? comparer;
+
+        public IScheduler? Scheduler { get; }
+
+        public SynchronizationContext? Context { get; }
+
+        public MultiDateTimePlotModel(IEqualityComparer<TKey>? comparer = null, IScheduler? scheduler = null, SynchronizationContext? synchronizationContext = null)
+        {
+            this.comparer = comparer;
+            this.Scheduler = scheduler;
+            this.Context = synchronizationContext;
+        }
+
+        protected virtual double Combine(double x0, double x1) => x1;
+
+        //public void Reset() => RemoveByPredicate(a => true);
+
+        //public void Remove(ISet<string> names) => RemoveByPredicate(s => names.Contains(s.Title));
+
+        public void OnCompleted()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error) => throw new NotImplementedException($"Error in {nameof(MultiDateTimePlotModel<TGroupKey, TKey>)}");
+
+        public void OnNext(KeyValuePair<TGroupKey, KeyValuePair<TKey, KeyValuePair<DateTime, double>>> value)
+        {
+            AddToDataPoints(value);
+            refreshSubject.OnNext(Unit.Default);
+        }
+
+        protected virtual void AddToDataPoints(KeyValuePair<TGroupKey, KeyValuePair<TKey, KeyValuePair<DateTime, double>>> item)
+        {
+            lock (Models)
+            {
+                (this as IMixedScheduler).ScheduleAction(() =>
+                {
+                    if (!Models.ContainsKey(item.Key))
+                    {
+                        var plotModel = new PlotModel();
+                        Models[item.Key] = new MultiDateTimeModel<TKey>(plotModel, this.comparer, this.Scheduler);
+                        PlotModelChanges.OnNext(plotModel);
+                    }
+                    Models[item.Key].OnNext(item.Value);
+                });
+            }
+        }
+
+        public IDisposable Subscribe(IObserver<PlotModel> observer)
+        {
+            return PlotModelChanges.Subscribe(observer.OnNext);
+        }
+    }
+}
