6aaf227bd3516bd142878c1711c593dbdd0a9f64
dtaylor-530 <d.taylor530@gmail.com>
2020-11-13 14:53:33 +0000

    Moves TimeKellyModel into library.

---
+++ OxyPlot.Reactive.DemoApp/Views/TimeKellyView.xaml.cs
@@ -0,0 +1,65 @@
+﻿using OxyPlot.Data.Common;
+using ReactiveUI;
+using System;
+using System.Windows;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Windows.Controls;
+using OxyPlot.Reactive.DemoApp.Common;
+using System.Reactive.Linq;
+using System.Collections.Generic;
+using OxyPlot.Reactive.Time;
+
+namespace OxyPlot.Reactive.DemoApp.Views
+{
+    /// <summary>
+    /// Interaction logic for TimeSeriesStatsView.xaml
+    /// </summary>
+    public partial class TimeKellyView : UserControl
+    {
+        CompositeDisposable disposable = new CompositeDisposable();
+
+        public TimeKellyView()
+        {
+            InitializeComponent();
+
+            //var model = new TimeKellyModel<string>(plotView1.Model ??= new PlotModel(), scheduler: RxApp.MainThreadScheduler);
+            var model2 = new Time2KellyModel<string>(plotView1.Model ??= new PlotModel(), scheduler: RxApp.MainThreadScheduler);
+
+            disposable = new CompositeDisposable();
+
+            GetData()
+                .Subscribe(a => model2.OnNext(KeyValuePair.Create("", (IKellyPoint<string>)a)))
+                .DisposeWith(disposable);
+
+            RatioComboBox.SelectItemChanges<double>().Subscribe(model2.OnNext);
+        }
+
+        static IObservable<KellyPoint<string>> GetData()
+        {
+            var array = Csv.Read()
+                .Select(a => new KellyPoint<string>(a.DateTime_, default, null, a.Odd, LayUnitProfit(a), ""))
+                .OrderBy(a=>a.Var)
+                .ToArray();
+
+            IObservable<KellyPoint<string>> cc = array.Take(2000).ToObservable().Merge(
+                array
+                .Skip(2000)
+                .ToObservable()
+                .Pace(TimeSpan.FromSeconds(0.1)))
+                 .Publish().RefCount();
+            return cc;
+        }
+
+        static double LayUnitProfit(CsvRow csvRow)
+        {
+            return csvRow.Profit > 0 ? 1 : -csvRow.Odd;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            disposable.Dispose();
+        }
+    }
+
+}
--- OxyPlot.Reactive.DemoApp/Views/TimeSeriesStatsView.xaml.cs
+++ OxyPlot.Reactive.DemoApp/Views/TimeSeriesStatsView.xaml.cs
@@ -5,14 +5,10 @@ using System;
 using System.Windows;
 using OxyPlot.Reactive.Model;
 using OxyPlot.Reactive.Model.Enum;
-using System.Linq;
 using System.Reactive.Disposables;
 using System.Windows.Controls;
 using OxyPlot.Reactive.DemoApp.Common;
 using System.Reactive.Linq;
-using OxyPlot.Reactive.DemoApp.Model;
-using System.Resources;
-using System.Collections.Generic;
 
 namespace OxyPlot.Reactive.DemoApp.Views
 {
@@ -31,51 +27,42 @@ namespace OxyPlot.Reactive.DemoApp.Views
             var model2 = new TimeOnTheFlyStatsModel<string>(plotView2.Model ??= new PlotModel(), scheduler: RxApp.MainThreadScheduler);
             var model3 = new TimeOnTheFlyStatsModel<string>(plotView3.Model ??= new PlotModel(), scheduler: RxApp.MainThreadScheduler);
             var model4 = new TimeOnTheFlyStatsModel<string>(plotView4.Model ??= new PlotModel(), scheduler: RxApp.MainThreadScheduler);
-            var model5 = new TimeKellyModel<string>(plotView5.Model ??= new PlotModel(), scheduler: RxApp.MainThreadScheduler);
-
+          
             disposable = new CompositeDisposable();
 
             TimeDataSource.Observe1000().Pace(TimeSpan.FromSeconds(0.5))
-               .SubscribeCustom3<ITimeTwoPoint<string, OnTheFlyStats.Stats>, ITimeTwoPoint<string, OnTheFlyStats.Stats>, OnTheFlyStats.Stats>(model1)
+               .SubscribeCustom3<ITimeModelPoint<string, OnTheFlyStats.Stats>, ITimeModelPoint<string, OnTheFlyStats.Stats>, OnTheFlyStats.Stats>(model1)
                .DisposeWith(disposable);
 
             TimeDataSource.Observe3()
-                .SubscribeCustom3<ITimeTwoPoint<string, OnTheFlyStats.Stats>, ITimeTwoPoint<string, OnTheFlyStats.Stats>, OnTheFlyStats.Stats>(model2)
-                     .DisposeWith(disposable);
-
-            TimeDataSource.Observe1000PlusMinus().Pace(TimeSpan.FromSeconds(0.2))
-               .SubscribeCustom3<ITimeTwoPoint<string, OnTheFlyStats.Stats>, ITimeTwoPoint<string, OnTheFlyStats.Stats>, OnTheFlyStats.Stats>(model3)
-                    .DisposeWith(disposable);
-
-            TimeDataSource.Observe1000PlusMinus().Pace(TimeSpan.FromSeconds(0.2))
-               .SubscribeCustom3<ITimeTwoPoint<string, OnTheFlyStats.Stats>, ITimeTwoPoint<string, OnTheFlyStats.Stats>, OnTheFlyStats.Stats>(model4)
-                    .DisposeWith(disposable);
-
-            TimeDataSource.Observe1000().Pace(TimeSpan.FromSeconds(0.5))
-        .SubscribeCustom3<ITimeTwoPoint<string, OnTheFlyStats.Stats>, ITimeTwoPoint<string, OnTheFlyStats.Stats>, OnTheFlyStats.Stats>(model1)
-        .DisposeWith(disposable);
-
-
-            var array = Csv.Read()
-                 .Select(a => new TimeKellyPoint<string>(a.DateTime_, null, null, null, null, a.Profit, a.Wager, a.Odd, 0, ""))
-                 .ToArray();
-
-            array.Take(1000).ToObservable().Merge(
-                array
-                .Skip(1000)
-                .ToObservable()
-                .Pace(TimeSpan.FromSeconds(0.1)))
-                .Subscribe(a => model5.OnNext(KeyValuePair.Create("", (ITimeKellyPoint<string>)a)))
+                .SubscribeCustom3<ITimeModelPoint<string, OnTheFlyStats.Stats>, ITimeModelPoint<string, OnTheFlyStats.Stats>, OnTheFlyStats.Stats>(model2)
                 .DisposeWith(disposable);
 
-            ComboBox1.SelectionChanged += (s, e) =>
+            TimeDataSource.Observe1000PlusMinus().Pace(TimeSpan.FromSeconds(0.2))
+               .SubscribeCustom3<ITimeModelPoint<string, OnTheFlyStats.Stats>, ITimeModelPoint<string, OnTheFlyStats.Stats>, OnTheFlyStats.Stats>(model3)
+               .DisposeWith(disposable);
+
+            TimeDataSource.Observe1000PlusMinus().Pace(TimeSpan.FromSeconds(0.2))
+               .SubscribeCustom3<ITimeModelPoint<string, OnTheFlyStats.Stats>, ITimeModelPoint<string, OnTheFlyStats.Stats>, OnTheFlyStats.Stats>(model4)
+               .DisposeWith(disposable);
+
+            TimeDataSource.Observe1000().Pace(TimeSpan.FromSeconds(0.5))
+                .SubscribeCustom3<ITimeModelPoint<string, OnTheFlyStats.Stats>, ITimeModelPoint<string, OnTheFlyStats.Stats>, OnTheFlyStats.Stats>(model1)
+                .DisposeWith(disposable);
+
+
+            ComboBox1.SelectItemChanges<RollingOperation>().Subscribe(rollingOperation =>
             {
-                var rollingOperation = e.AddedItems.Cast<RollingOperation>().Single();
                 model1.OnNext(rollingOperation);
                 model2.OnNext(rollingOperation);
                 model3.OnNext(rollingOperation);
                 model4.OnNext(rollingOperation);
-            };
+            });
+        }
+
+        static double LayUnitProfit(CsvRow csvRow)
+        {
+            return csvRow.Profit > 0 ? 1 : -csvRow.Profit;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
